a78732271ab3cf6643f6fcc9054fa73ec87053cf
03friska <fmaralantang@gmail.com>
2024-05-18 23:10:20 +0530

    Login dan landingPage

---
+++ frontend/src/javascript/landing_page.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
